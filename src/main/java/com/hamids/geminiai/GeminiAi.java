ec04e51ad523aa551bc2f713f1e01edeb08f9a9b
Hamid Ali Khan <aleehamid1024@gmail.com>
2026-01-01 16:23:17 +0500

    initial commits

---
+++ src/main/java/com/hamids/geminiai/GeminiAi.java
@@ -0,0 +1,13 @@
+package com.hamids.geminiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiAi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeminiAi.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamids/geminiai/GeminiAiTests.java
@@ -0,0 +1,13 @@
+package com.hamids.geminiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiAiTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
