bd5d5e42cafe610fac751e0b2c68299c57a0f27a
Hamid Ali Khan <aleehamid1024@gmail.com>
2026-02-16 01:06:41 +0500

    Update CORS configuration to allow requests from the new domain

--- src/main/java/com/hamids/geminiai/config/CorsConfig.java
+++ src/main/java/com/hamids/geminiai/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://hamids-ai.vercel.app")
+                        .allowedOrigins("https://hamids-ai.hmd02.tech")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
